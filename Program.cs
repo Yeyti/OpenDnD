34ff7e8a35cb55665f735c4a0be89277cfb8d027
EDinc <eternaldreaminc@gmail.com>
2018-07-15 22:47:10 +0300

    all
    Signed-off-by: EDinc <eternaldreaminc@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dndproxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
